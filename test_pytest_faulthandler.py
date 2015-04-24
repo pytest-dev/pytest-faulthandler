b49391f49b62fbfd980abbd8b82e342d004fcdce
Bruno Oliveira <nicoddemus@gmail.com>
2015-04-24 04:33:19 -0300

    faulthandler able to display crashes even after pytest shutdowns
    fixes #3

--- test_pytest_faulthandler.py
+++ test_pytest_faulthandler.py
@@ -1,6 +1,8 @@
 pytest_plugins = ['pytester']
 
+
 def test_enabled(testdir):
+    """Test single crashing test displays a traceback."""
     testdir.makepyfile('''
     import faulthandler
     def test_crash():
@@ -10,9 +12,29 @@ def test_enabled(testdir):
     result.stderr.fnmatch_lines([
         "*Fatal Python error*"
     ])
+    assert result.ret != 0
+
+
+def test_crash_near_exit(testdir):
+    """Test that fault handler displays crashes that happen even after
+    pytest is exiting (for example, when the interpreter is shutting down).
+    """
+    testdir.makepyfile('''
+    import faulthandler
+    import atexit
+    def test_ok():
+        atexit.register(faulthandler._sigabrt)
+    ''')
+    result = testdir.runpytest()
+    result.stderr.fnmatch_lines([
+        "*Fatal Python error*"
+    ])
+    assert result.ret != 0
 
 
 def test_disabled(testdir):
+    """Test option to disable fault handler in the command line.
+    """
     testdir.makepyfile('''
     import faulthandler
     def test_disabled():
@@ -22,3 +44,4 @@ def test_disabled(testdir):
     result.stdout.fnmatch_lines([
         "*1 passed*"
     ])
+    assert result.ret == 0
