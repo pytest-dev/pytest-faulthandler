2cb9ba92e640fa5ff871b475db3443075f3d5f89
Bruno Oliveira <nicoddemus@gmail.com>
2015-11-23 15:12:00 -0200

    Update development status to "production" in setup.py and remove python 2.6 as supported

--- setup.py
+++ setup.py
@@ -23,11 +23,10 @@ setup(
     },
     keywords='pytest faulthandler',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
