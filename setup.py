1b27024821b19ff1a1648de9cc5b9b4e4f92ce25
Bruno Oliveira <nicoddemus@gmail.com>
2016-11-16 17:13:52 -0200

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name='pytest-faulthandler',
-    version='1.3.0',
+    version='1.3.1',
     py_modules=['pytest_faulthandler'],
     url='https://github.com/pytest-dev/pytest-faulthandler',
     license='MIT',
